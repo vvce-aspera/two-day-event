551eb34b5efa56578bc81cfe185fb281c8f09dd9
Suraj Govind <surajgovind332000@gmail.com>
2020-07-15 20:03:24 +0530

    Updates

---
+++ precache-manifest.e735347bd4b73bc1c7222de811adb57f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af8f0629cde4f6e61792735f1a2faa4",
+    "url": "/two-day-event/index.html"
+  },
+  {
+    "revision": "6d0487b842188adcdb12",
+    "url": "/two-day-event/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "c7ed0e4db09a54c377e7",
+    "url": "/two-day-event/static/css/main.344531c1.chunk.css"
+  },
+  {
+    "revision": "6d0487b842188adcdb12",
+    "url": "/two-day-event/static/js/2.3a43e024.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/two-day-event/static/js/2.3a43e024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ed0e4db09a54c377e7",
+    "url": "/two-day-event/static/js/main.b2e15b94.chunk.js"
+  },
+  {
+    "revision": "343281d46086b86c57c5",
+    "url": "/two-day-event/static/js/runtime-main.539eb896.js"
+  }
+]);
