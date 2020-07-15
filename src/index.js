b7ba06cf955b33f86bc077bd46b34f1c918aa56e
Suraj-Gov <surajgovind332000@gmail.com>
2020-07-15 14:15:19 +0530

    minimal code, styling required

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import PosterSection from "./PosterSection";
 
 class Main extends Component {
   componentDidMount() {
@@ -12,7 +13,23 @@ class Main extends Component {
   }
 
   render() {
-    return <h1>hello</h1>;
+    return (
+      <div className="container">
+        <h1>TWO DAY EVENT</h1>
+        <h4>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et nibh
+          dictum, condimentum metus eget, tristique elit.
+        </h4>
+        <PosterSection
+          img={`${process.env.PUBLIC_URL}/images/keynoteWeb.jpg`}
+          code="XXX-XXX-XXX"
+          coordinators={["Jane Doe", "Jack Blanc"]}
+          platform="Instagram"
+          timings="10am"
+          key="1"
+        />
+      </div>
+    );
   }
 }
 
